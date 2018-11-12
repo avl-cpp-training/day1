9b5f5a8af3dd225bdbb079d32413818cd19177f9
Jan Kelemen <jkeleme@gmail.com>
2018-11-12 20:07:17 +0100

    solution for 2nd exercise

--- exercise/02/code.cpp
+++ exercise/02/code.cpp
@@ -1,2 +1,15 @@
 #include "stdafx.h"
 #include "code.h"
+
+#include <algorithm>
+
+void f1(int& a, int& b)
+{
+    std::swap(a, b);
+}
+
+void f2(int* const begin, int* const end)
+{
+    for (auto it = begin; it != end; ++it)
+        ++(*it);
+}
