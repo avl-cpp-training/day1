c3cd3554e2d3b918c03d896cd6a404f50ba14ade
Gordan Kucinar <gordan.kucinar@avl.com>
2018-11-12 16:58:55 +0100

    Done with exception

--- exercise/01/app/dynamic_link.cpp
+++ exercise/01/app/dynamic_link.cpp
@@ -1,12 +1,17 @@
 #include "pch.h"
 #include "dynamic_link.h"
 #include <Windows.h>
+#include <exception>
 
 
 double no_implib::get_value(int input)
 {
   using get_value_func = double(*)(int) ;
   HMODULE module = LoadLibraryA("dynamic_library.dll");
+  if (!module)
+    throw "no dll";
   get_value_func func = (get_value_func)GetProcAddress(module, "get_value");
+  if (!func)
+    throw "no get_value function found in dynamic_library.dll";
   return func(input);
 }
