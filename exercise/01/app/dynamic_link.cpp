96fe2ce0e3947ce4e09b29d39a4670320d2de7f8
Gordan Kucinar <gordan.kucinar@avl.com>
2018-11-12 22:39:20 +0100

    Zaboravio FreeLibrary

--- exercise/01/app/dynamic_link.cpp
+++ exercise/01/app/dynamic_link.cpp
@@ -4,13 +4,23 @@
 #include <exception>
 
 
+
 double no_implib::get_value(int input)
 {
+  struct scoped_library
+  {
+    HMODULE module;
+    scoped_library()
+    {
+      module = LoadLibraryA("dynamic_library.dll");
+    }
+    ~scoped_library() { if(module) FreeLibrary(module); }
+
+  } loader;
   using get_value_func = double(*)(int) ;
-  HMODULE module = LoadLibraryA("dynamic_library.dll");
-  if (!module)
+  if (!loader.module)
     throw "no dll";
-  get_value_func func = (get_value_func)GetProcAddress(module, "get_value");
+  get_value_func func = (get_value_func)GetProcAddress(loader.module, "get_value");
   if (!func)
     throw "no get_value function found in dynamic_library.dll";
   return func(input);
