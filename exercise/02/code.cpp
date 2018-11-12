c5ee92a6c6a13a897ab4be694c6b73d59edfdd1e
Marina Krček <marinakrco7@gmail.com>
2018-11-12 23:51:25 +0100

    Exercise 02
    - uncomment function calls in unit tests
    - implement functions according to the unit tests

--- exercise/02/code.cpp
+++ exercise/02/code.cpp
@@ -1,2 +1,18 @@
 #include "stdafx.h"
 #include "code.h"
+
+void f1(int& a, int& b)
+{
+	a = a + b;
+	b = a - b;
+	a = a - b;
+}
+
+void f2(int* begin, int* end)
+{
+	while (begin != end)
+	{
+		(*begin) += 1;
+		begin++;
+	}
+}
