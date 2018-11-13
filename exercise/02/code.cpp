61639b210e83c05907a6c542e7a75c57f673c785
tmalkoc <Tvrtko.Malkoc@avl.com>
2018-11-13 11:14:12 +0100

    Tvrtko Malkoc - day1 - exercise 02

--- exercise/02/code.cpp
+++ exercise/02/code.cpp
@@ -1,2 +1,15 @@
 #include "stdafx.h"
 #include "code.h"
+
+void f1(int& a, int& b)
+{
+  a += b;
+  b = a - b;
+  a = a - b;
+}
+
+void f2(int *begin, const int *const end)
+{
+  for (; begin < end; begin++)
+    (*begin)++;
+}
