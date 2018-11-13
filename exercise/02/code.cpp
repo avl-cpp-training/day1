30d55c6865fb3a4d77ed57355c9da86692ded876
Luka Križan <luka.krizan@avl.com>
2018-11-13 11:05:03 +0100

    Finished second exercise

--- exercise/02/code.cpp
+++ exercise/02/code.cpp
@@ -1,2 +1,17 @@
 #include "stdafx.h"
 #include "code.h"
+
+void f1(int& a, int& b)
+{
+  using std::swap;
+  swap(a, b);
+}
+
+void f2(int* begin, int* end)
+{
+  while (begin < end)
+  {
+    ++(*begin);
+    ++begin;
+  }
+}
