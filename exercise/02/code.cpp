154de8cb1e1691a491a734c2bc6dde0759b75a20
imaras <imaras@net.hr>
2018-11-13 11:18:55 +0100

    day1 - exercise 02 - Unit testovi za jednostavne funkcije

--- exercise/02/code.cpp
+++ exercise/02/code.cpp
@@ -1,2 +1,18 @@
 #include "stdafx.h"
 #include "code.h"
+
+void f1(int& a, int& b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+void f2(int* begin, int* end)
+{
+	while (end != begin)
+	{
+		*begin = (*begin) + 1;
+		begin++;
+	}
+}
