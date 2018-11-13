1b704d6b5fac8103aae068aca79a0e948361c3ea
Ivona Mramor <ivona.mramor@avl.com>
2018-11-13 11:15:46 +0100

    Exercise2

--- exercise/02/code.cpp
+++ exercise/02/code.cpp
@@ -1,2 +1,15 @@
 #include "stdafx.h"
 #include "code.h"
+
+void f1(int& a,int& b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+	
+}
+void f2(int* begin, const int* const end)
+{
+	for (; begin < end; begin++)
+		(*begin)++;
+}
