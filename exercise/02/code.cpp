adb2e8983f9919972e7bf75b99efbaf6f78957cc
mijo.cikes <mijo.cikes@avl.com>
2018-11-13 16:16:42 +0100

    day 1

--- exercise/02/code.cpp
+++ exercise/02/code.cpp
@@ -1,2 +1,15 @@
 #include "stdafx.h"
 #include "code.h"
+void f2(int* a1, int* a2)
+{
+	int diff = a2 - a1;
+	for (int i = 0; i < diff; i++)
+		*(a1 + i) = *(a1 + i) + 1;
+}
+
+void f1(int &a, int &b)
+{
+	int t = a;
+	a = b;
+	b = t;
+}
