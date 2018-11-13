356a941c8a0cf6ab26dbd2ace7fefb0e3b6c03de
Ivan Kvesić <ivan.kvesic@fer.hr>
2018-11-13 11:15:41 +0100

    Exercise 02 solved

--- exercise/02/test1.cpp
+++ exercise/02/test1.cpp
@@ -14,14 +14,22 @@ namespace simple_test
 		TEST_METHOD(f1_changes_values)
 		{
 			int a=1, b=2;
-			//f1(a, b);
+			f1(a, b);
 			Assert::AreEqual(2, a);
 			Assert::AreEqual(1, b);
+		}
+
+		TEST_METHOD(f1_change_values_second_time) 
+		{
+			int a = 5, b = 15;
+			f1(a, b);
+			Assert::AreEqual(15, a);
+			Assert::AreEqual(5, b);
 		}
 		TEST_METHOD(f2_increases_all_values_by_one)
 		{
 			int arr[] = { 1, 2, 3 };
-			//f2(arr, arr+3);
+			f2(arr, arr+3);
 			Assert::AreEqual(2, arr[0]);
 			Assert::AreEqual(3, arr[1]);
 			Assert::AreEqual(4, arr[2]);
@@ -29,7 +37,7 @@ namespace simple_test
 		TEST_METHOD(f2_works_for_different_number_of_values)
 		{
 			int arr[] = { 2, 4, 6, 8, 10 };
-			//f2(arr, arr+5);
+			f2(arr, arr+5);
 			Assert::AreEqual(3, arr[0]);
 			Assert::AreEqual(5, arr[1]);
 			Assert::AreEqual(7, arr[2]);
