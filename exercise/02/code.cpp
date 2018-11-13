3fcc8c7164e2ab910b8e9dd9f26b8034ed44b8ce
Dean Leibner <Dean.Leibner@avl.com>
2018-11-13 11:13:07 +0100

    Solution to exercise 02

--- exercise/02/code.cpp
+++ exercise/02/code.cpp
@@ -1,2 +1,17 @@
 #include "stdafx.h"
 #include "code.h"
+
+void f1 (int &a, int& b)
+{
+  int temp = a;
+  a = b;
+  b = temp;
+}
+
+void f2(int* const begin, int* const end)
+{
+  for (auto i=begin; i < end; ++i)
+  {
+    ++*i;
+  }
+}
