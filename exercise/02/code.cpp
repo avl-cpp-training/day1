bd1035e6d6f0517452058de83799a58e1032d6bc
busar <renato.busa@avl.com>
2018-11-13 17:06:17 +0100

    Day1: - exercise 01 - korištenje statičke biblioteke (.lib) i dinamičke biblioteke (.dll, na dva načina) - exercise 02 - Unit testovi za jednostavne funkcije

--- exercise/02/code.cpp
+++ exercise/02/code.cpp
@@ -1,2 +1,17 @@
 #include "stdafx.h"
 #include "code.h"
+
+
+
+void f1(int &a, int &b)
+{
+  int temp = a;
+  a = b;
+  b = temp;
+}
+
+void f2(int *b, int *e)
+{
+  for (; b < e; ++b)
+    (*b)++;
+}
---
+++ exercise/02/code.h
@@ -0,0 +1,2 @@
+void f1(int &a, int &b);
+void f2(int *b, int *e);
