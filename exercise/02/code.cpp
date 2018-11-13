e1f7e894cc7d91a8e35741c42ed00bc63e72765e
maja <maja23miljak@gmail.com>
2018-11-13 18:39:18 +0100

    exercise 2

--- exercise/02/code.cpp
+++ exercise/02/code.cpp
@@ -1,2 +1,16 @@
 #include "stdafx.h"
 #include "code.h"
+
+void f1(int& a, int& b)
+{
+	int tmp = a;
+	a = b;
+	b = tmp;
+}
+
+void f2(int* first, int* last)
+{
+	for (; first < last; ++first)
+		++(*first);
+}
+
---
+++ exercise/02/code.h
@@ -0,0 +1,3 @@
+
+void f1(int& a, int& b);
+void f2(int* first, int* last);
